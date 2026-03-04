ccbb42be58225152955fc30b9e088ad751ce4a59
Phil-the-amature-dev <98315948+Phil-the-amature-dev@users.noreply.github.com>
2026-03-04 22:02:35 +0100

    rocket cooldown

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -10,8 +10,11 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] private float mousSens;
     [SerializeField] private float VerticalCamMaxClamp;
     [SerializeField] private float VerticalCamMinClamp;
+    [SerializeField] private float rocketCooldown;
 
     private float camRotation;
+    private bool canShoot = true;
+    private float lastShot;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,12 +32,22 @@ public class PlayerControls : MonoBehaviour
         camRotation = Mathf.Clamp(camRotation, VerticalCamMinClamp, VerticalCamMaxClamp);
         cameraPivot.transform.localRotation = Quaternion.Euler(camRotation, 0, 0);
 
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Time.time > lastShot + rocketCooldown && !canShoot) { canShoot = true; }
+        
+        if (Input.GetKeyDown(KeyCode.Mouse0) && canShoot)
         {
             Instantiate(rocket, rocketSpawn.transform.position, cameraPivot.transform.rotation);
+            ResetRocketCooldown();
+            
         }
 
 
 
+    }
+
+    private void ResetRocketCooldown()
+    {
+        canShoot = false;
+        lastShot = Time.time; 
     }
 }
